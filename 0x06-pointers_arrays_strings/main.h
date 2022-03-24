dae8d68f29a1f2c41a07eecba30337b80f26844f
RonaldOtieno <ronnieollimo@gmail.com>
2022-03-24 08:34:16 +0300

    commit code

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+1 char *_strcat(char *dest, char *src);
+  2 char *_strncat(char *dest, char *src, int n);
+  3 char *_strncpy(char *dest, char *src, int n);
+  4 int _strcmp(char *s1, char *s2);
+  5 void reverse_array(int *a, int n);
+  6 char *string_toupper(char *);
+  7 char *cap_string(char *);
+  8 char *leet(char *);
+  9 char *rot13(char *);
+ 10 void print_number(int n);
+ 11 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+ 12 void print_buffer(char *b, int size);
+ 13 int _putchar(char c);
+
